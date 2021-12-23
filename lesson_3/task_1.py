7a56a7046875ddb3c27ef6e03ab9df9933f74c05
Alenanka <89450782+Alenanka@users.noreply.github.com>
2021-12-23 23:17:59 +0300

    Update task_1.py

--- lesson_3/task_1.py
+++ lesson_3/task_1.py
@@ -1,11 +1,11 @@
+import pymongo
 from bs4 import BeautifulSoup as bs
 import requests
 from pymongo import MongoClient
-
+import re
 client = MongoClient('localhost',27017)
 db = client['parser_position']
 vacancy = db['vacancy']
-
 main_url = 'https://hh.ru/search/vacancy'
 search_position = input('Введите должность:')
 while 1:
@@ -23,8 +23,10 @@ params = {'text': search_position,
 list_of_position =[]
 
 def insert_vacancy(vak):
-    if not vacancy.find_one({'link': vak['link']}):
-        vacancy.insert_one(vak)
+ try:
+     vacancy.insert_one(vak)
+ except pymongo.errors.DuplicateKeyError:
+     pass
 
 
 def search_vacancy(sal):
@@ -40,6 +42,7 @@ while params['page'] < count_page:
         for position in positions:
             pos_vok = {}
             link = position.find('a',{'data-qa':"vacancy-serp__vacancy-title"}).get('href')
+            id = re.search(r'\d+',link).group()
             name = position.find('a',{'data-qa':"vacancy-serp__vacancy-title"}).text
             try:
                 salary = position.find('span',{'data-qa':"vacancy-serp__vacancy-compensation"}).text.split(' ')
@@ -53,13 +56,14 @@ while params['page'] < count_page:
                     salary_currency = salary[2].replace('\xa0', '')
                 elif salary[0] == 'до':
                     salary_min = None
-                    salary_min = int(salary[1].replace('\u202f', ''))
+                    salary_max = int(salary[1].replace('\u202f', ''))
                     salary_currency = salary[2].replace('\xa0', '')
             except:
                 salary_min = None
                 salary_max = None
                 salary_currency = None
 
+            pos_vok['_id'] = id
             pos_vok['name'] = name
             pos_vok['link'] = link
             pos_vok['salary_min'] = salary_min
@@ -68,9 +72,9 @@ while params['page'] < count_page:
             insert_vacancy(pos_vok)
             list_of_position.append(pos_vok)
     params['page'] += 1
-#
-# for doc in vacancy.find():
-#     print(doc)
+
+for doc in vacancy.find():
+    print(doc)
 
 salary = int(input('ВВедите от какой зарплаты вывести вакансии:'))
 search_vacancy(salary)
